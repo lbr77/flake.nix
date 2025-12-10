178f664b838cf8d244e7c3e4620bd72c18ef1b1d
libr <me@nvme0n1p.dev>
2025-12-10 22:04:42 +0800

    Update at 2025-12-10 22:04:42

--- pkgs/codex.nix
+++ pkgs/codex.nix
@@ -7,12 +7,12 @@
 
 stdenv.mkDerivation rec {
     pname = "codex";
-    version = "rust-v0.65.0";
+    version = "rust-v0.66.0";
 
     src = fetchurl {
         url = "https://github.com/openai/codex/releases/download/${version}/codex-aarch64-apple-darwin.tar.gz";
-        sha256 = "sha256-UBc9J7v6GTrMu6gfz9NA1V3118NRfp2dB5g1kJOP3KE=";
-        # sha256 = lib.fakeSha256;
+        # sha256 = "sha256-UBc9J7v6GTrMu6gfz9NA1V3118NRfp2dB5g1kJOP3KE=";
+        sha256 = lib.fakeSha256;
     };
 
     nativeBuildInputs = [ gnutar ];
